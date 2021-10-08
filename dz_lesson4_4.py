0e244866961f586e80833fa8693cc48ceaf72c1a
AlexIvanovCoder <64576227+AlexIvanovCoder@users.noreply.github.com>
2021-10-08 22:45:00 +0300

    Delete dz_lesson4_4.py

--- dz_lesson4_4.py
+++
@@ -1,7 +0,0 @@
-﻿# 4
-list_of_numbers = [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11]
-filtered_list_of_numbers = []
-
-[filtered_list_of_numbers.append(i) for i in list_of_numbers if i not in filtered_list_of_numbers]
-
-print(filtered_list_of_numbers)
